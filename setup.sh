49943f4a991eae4c3cf518bee0fb13112ee2e756
Robin Melanson <remcogold@gmail.com>
2019-02-22 14:52:18 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,7 @@
 # INSTALL REQUIRED DEVELOPMENT LIBRARIES
 ./install/installJava8.sh
 
+# INSTALL STOCK WIDGETS TEST DATA
+./install/bootstraps/swTestDataBootstrap.sh
+
 java -jar ./jars/token-requestor-0.1.0.jar &
