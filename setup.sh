0a77905e4fa70c13992e817aa4a5250223481009
Robin Melanson <remcogold@gmail.com>
2019-03-01 07:04:34 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,15 +2,6 @@
 # Setup the required environment
 . ./env/setEnv.sh
 
-# INSTALL REQUIRED DEVELOPMENT LIBRARIES
-./installs/installJava8.sh
-
-# INSTALL STOCK WIDGETS TEST DATA
-./installs/bootstraps/swTestDataBootstrap.sh
-
-# SET UP LINUX ADMIN USER
-./installs/addLinuxUser.sh $owner $group $home $pkg
-
 # CREATE SERVICE APPLICATION DIRECTORY
 mkdir -p $appDir
 
