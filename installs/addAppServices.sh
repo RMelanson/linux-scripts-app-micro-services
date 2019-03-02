ee39ab13766e72c36b035b72fd85bd3202183356
Robin Melanson <remcogold@gmail.com>
2019-03-02 00:16:20 -0500

    Update addAppServices.sh

--- installs/addAppServices.sh
+++ installs/addAppServices.sh
@@ -1,4 +1,4 @@
 #Set appServices.d as an init.d service
-echo y|cp -rf appServices.d /etc/init.d/appServices.d
+echo y|cp -rf installs/appServices.d /etc/init.d/appServices.d
 chkconfig appServices.d on
 service appServices.d start
