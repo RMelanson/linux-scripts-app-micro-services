ea18bec652a44e7d55d0823d5879f417e568afa1
Robin Melanson <remcogold@gmail.com>
2019-03-02 12:08:50 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,18 @@
 #! /bin/bash
-# Setup the required environment
+scriptName=setup.sh:
+# SETUP THE REQUIRED ENVIRONMENTt
+echo $scriptName EXECUTING ./env/setEnv.sh
 . ./env/setEnv.sh
 
 # CREATE SERVICE APPLICATION DIRECTORY
+echo $scriptName EXECUTING mkdir -p $appProcessDir
 mkdir -p $appProcessDir
 
 # INSTALL JAVA APPS TO SERVER
+echo $scriptName EXECUTING cp ./installs/appServices.d $appServicesDir
 cp ./installs/appServices.d $appServicesDir
 chmod 766 $appServicesDir/$prog
 
 # INSTALL SWAPIs as a SERVICE
+echo $scriptName EXECUTING ./installs/addAppServices.sh
 ./installs/addAppServices.sh
