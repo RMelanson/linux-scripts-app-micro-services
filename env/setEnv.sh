a379ab517bbf47b6cc012b8fe4d7a9a13b2a456d
Robin Melanson <remcogold@gmail.com>
2019-03-06 15:28:06 -0500

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -3,4 +3,4 @@ pkg=app
 appServicesDir=/etc/init.d
 appProcessDir=$appServicesDir/appProcesses
 daemon=appServices.d
-testFile=$appProcessDir/springBootHelloWorldDemo-0.1.0.jar
+testApp=" java -jar $appProcessDir/springBootHelloWorldDemo-0.1.0.jar"
