8db1a9351665d8067cb0577f431ca89fdf13ccd7
Robin Melanson <remcogold@gmail.com>
2019-03-06 11:27:22 -0500

    Update addAppServices.sh

--- installs/addAppServices.sh
+++ installs/addAppServices.sh
@@ -1,13 +1,15 @@
-#Set $prog as an init.d service
+#Set $daemon as an init.d service
 
-echo $scriptName Set $prog as an init.d service
+scriptName=addAppServices.sh
+
+echo $scriptName Set $daemon as an init.d service
 scriptName=addAppServices.sh:
 
-echo $scriptName EXECUTING echo y|cp -rf installs/appServices.d $appServicesDir$prog
-echo y|cp -rf installs/appServices.d $appServicesDir$prog
+echo $scriptName EXECUTING echo y|cp -rf installs/appServices.d $appServicesDir$daemon
+echo y|cp -rf installs/appServices.d $appServicesDir$daemon
 
-echo $scriptName EXECUTING chkconfig $prog on
-chkconfig $prog on
+echo $scriptName EXECUTING chkconfig $daemon on
+chkconfig $daemon on
 
-echo $scriptName EXECUTING service $prog start
-service $prog start
+echo $scriptName EXECUTING service $daemon start
+service $daemon start
