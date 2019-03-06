cb3df0e4a2ad835c05fcb8861a98e3d32ec82171
Robin Melanson <remcogold@gmail.com>
2019-03-06 14:18:41 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,18 +4,29 @@ scriptName=setup.sh:
 echo $scriptName EXECUTING ./env/setEnv.sh
 . ./env/setEnv.sh
 
+echo Here 1 testfile = $testFile
+
 # CREATE SERVICE APPLICATION DIRECTORY
 echo $scriptName EXECUTING mkdir -p $appProcessDir
 mkdir -p $appProcessDir
+
+
+echo Here 2 testfile = $testFile
 
 # INSTALL JAVA APPS TO SERVER
 echo $scriptName EXECUTING cp ./installs/appServices.d $appServicesDir$daemon
 cp ./installs/appServices.d $appServicesDir$daemon
 chmod 766 $appServicesDir$daemon
 
+
+echo Here 3 testfile = $testFile
+
 # COPY SAMPLE TEST PROGRAM
 echo $scriptName EXECUTING cp ./jars/springBootHelloWorldDemo-0.1.0.jar $appProcessDir
 cp ./jars/springBootHelloWorldDemo-0.1.0.jar $appProcessDir
+
+
+echo Here 4 testfile = $testFile
 
 # INSTALL SWAPIs as a SERVICE
 echo $scriptName EXECUTING ./installs/addAppServices.sh
