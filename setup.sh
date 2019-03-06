f3c508457b03c152eb9eefeb20d4d375d5cbd50e
Robin Melanson <remcogold@gmail.com>
2019-03-06 11:32:23 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,6 +13,10 @@ echo $scriptName EXECUTING cp ./installs/appServices.d $appServicesDir$daemon
 cp ./installs/appServices.d $appServicesDir$daemon
 chmod 766 $appServicesDir$daemon
 
+# COPY SAMPLE TEST PROGRAM
+echo $scriptName EXECUTING cp ./jars/springBootHelloWorldDemo-0.1.0.jar $appServicesDir
+cp ./jars/springBootHelloWorldDemo-0.1.0.jar $appServicesDir
+
 # INSTALL SWAPIs as a SERVICE
 echo $scriptName EXECUTING ./installs/addAppServices.sh
 . ./installs/addAppServices.sh
