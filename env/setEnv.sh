a64beaacf502ed011cbc0925467d2fd1c6398487
Robin Melanson <remcogold@gmail.com>
2019-03-06 15:04:03 -0500

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -1,6 +1,6 @@
 # Microservices Environment
 pkg=app
-appServicesDir=/etc/init.d/
+appServicesDir=/etc/init.d
 appProcessDir=$appServicesDir/appProcesses
 daemon=appServices.d
 testFile=$appProcessDir/springBootHelloWorldDemo-0.1.0.jar
