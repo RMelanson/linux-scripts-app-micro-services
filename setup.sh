77d7515c8d41ad47a08d454b0d336728662e9d91
Robin Melanson <remcogold@gmail.com>
2019-03-05 23:56:31 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,9 +9,9 @@ echo $scriptName EXECUTING mkdir -p $appProcessDir
 mkdir -p $appProcessDir
 
 # INSTALL JAVA APPS TO SERVER
-echo $scriptName EXECUTING cp ./installs/appServices.d $appServicesDir$prog
-cp ./installs/appServices.d $appServicesDir$prog
-chmod 766 $appServicesDir$prog
+echo $scriptName EXECUTING cp ./installs/appServices.d $appServicesDir$daemon
+cp ./installs/appServices.d $appServicesDir$daemon
+chmod 766 $appServicesDir$daemon
 
 # INSTALL SWAPIs as a SERVICE
 echo $scriptName EXECUTING ./installs/addAppServices.sh
