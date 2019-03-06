35081fbf320aed3e1a998bf89ecf9554d0cef596
Robin Melanson <remcogold@gmail.com>
2019-03-06 14:19:50 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,6 @@ echo Here 1 testfile = $testFile
 echo $scriptName EXECUTING mkdir -p $appProcessDir
 mkdir -p $appProcessDir
 
-
 echo Here 2 testfile = $testFile
 
 # INSTALL JAVA APPS TO SERVER
@@ -18,16 +17,16 @@ echo $scriptName EXECUTING cp ./installs/appServices.d $appServicesDir$daemon
 cp ./installs/appServices.d $appServicesDir$daemon
 chmod 766 $appServicesDir$daemon
 
-
 echo Here 3 testfile = $testFile
 
 # COPY SAMPLE TEST PROGRAM
 echo $scriptName EXECUTING cp ./jars/springBootHelloWorldDemo-0.1.0.jar $appProcessDir
 cp ./jars/springBootHelloWorldDemo-0.1.0.jar $appProcessDir
 
-
 echo Here 4 testfile = $testFile
 
 # INSTALL SWAPIs as a SERVICE
 echo $scriptName EXECUTING ./installs/addAppServices.sh
 . ./installs/addAppServices.sh
+
+echo Here 5 testfile = $testFile
