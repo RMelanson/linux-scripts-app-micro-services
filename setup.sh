dcb674651337dad3bc83cd3dd19f4fe98b243a22
Robin Melanson <remcogold@gmail.com>
2019-03-06 16:05:13 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,9 +9,9 @@ echo $scriptName EXECUTING mkdir -p $appProcessDir
 mkdir -p $appProcessDir
 
 # INSTALL JAVA APPS TO SERVER
-echo $scriptName EXECUTING cp ./installs/appServices.d $appServicesDir$daemon
-cp ./installs/appServices.d $appServicesDir$daemon
-chmod 766 $appServicesDir$daemon
+echo $scriptName EXECUTING cp ./installs/appServices.d $appServicesDir/$daemon
+cp ./installs/appServices.d $appServicesDir/$daemon
+chmod 766 $appServicesDir/$daemon
 
 # COPY SAMPLE TEST PROGRAM
 echo $scriptName EXECUTING cp ./jars/springBootHelloWorldDemo-0.1.0.jar $appProcessDir
