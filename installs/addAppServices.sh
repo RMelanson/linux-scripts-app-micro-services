b4c1af7100eedbc9cd80117c6fb5460745336a81
Robin Melanson <remcogold@gmail.com>
2019-03-07 13:50:38 -0500

    Update addAppServices.sh

--- installs/addAppServices.sh
+++ installs/addAppServices.sh
@@ -10,6 +10,3 @@ echo y|cp -rf installs/appServices.d $appServicesDir$daemon
 
 echo $scriptName EXECUTING chkconfig $daemon on
 chkconfig $daemon on
-
-echo $scriptName EXECUTING service $daemon start $testApp
-service $daemon start $testApp
