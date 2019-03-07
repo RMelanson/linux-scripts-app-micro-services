927cd782a69f2f69f920d598006796b120989645
Robin Melanson <remcogold@gmail.com>
2019-03-07 14:25:58 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,27 @@
 #! /bin/bash
-scriptName=setup.sh:
+setupName=setup.sh:
 # SETUP THE REQUIRED ENVIRONMENTt
-echo $scriptName EXECUTING ./env/setEnv.sh
+echo setup.sh: EXECUTING ./env/setEnv.sh
 . ./env/setEnv.sh
 
 # CREATE SERVICE APPLICATION DIRECTORY
-echo $scriptName EXECUTING mkdir -p $appProcessDir
+echo $setupName EXECUTING mkdir -p $appProcessDir
 mkdir -p $appProcessDir
 
 # INSTALL JAVA APPS TO SERVER
-echo $scriptName EXECUTING cp ./installs/appServices.d $appServicesDir/$daemon
+echo $setupName EXECUTING cp ./installs/appServices.d $appServicesDir/$daemon
 cp ./installs/appServices.d $appServicesDir/$daemon
 chmod 766 $appServicesDir/$daemon
 
 # COPY SAMPLE TEST PROGRAM
-echo $scriptName EXECUTING cp ./jars/springBootHelloWorldDemo-0.1.0.jar $appProcessDir
+echo $setupName EXECUTING cp ./jars/springBootHelloWorldDemo-0.1.0.jar $appProcessDir
 cp ./jars/springBootHelloWorldDemo-0.1.0.jar $appProcessDir
 
 # INSTALL $daemon as a SERVICE DEAMON
-echo BEFORE $scriptName EXECUTING service $daemon start $testApp
-echo $scriptName EXECUTING ./installs/addAppServices.sh
+echo BEFORE $setupName EXECUTING service $daemon start $testApp
+echo $setupName EXECUTING ./installs/addAppServices.sh
 . ./installs/addAppServices.sh
 
 # INSTALL TEST APP as a SERVICE
-echo AFTER $scriptName EXECUTING service $daemon start $testApp
+echo $setupName EXECUTING service $daemon start $testApp
 service $daemon start $testApp
