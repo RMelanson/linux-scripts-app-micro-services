d2a4b29d16f5efb3f74f22349a429e2572fa781a
Robin Melanson <remcogold@gmail.com>
2019-03-14 21:18:59 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -2,6 +2,8 @@
 
 # MICROSERVICES CONFIGURATION PARAMETERS
 pkg=appServices
+gitRepo="linux-scripts-app-services"
+installDir="/tmp/scripts/utils/appServices"
 appServicesDir=/etc/init.d
 appProcessDir=$appServicesDir/appProcesses
 daemon=appServices.d
