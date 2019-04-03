a06d4d282504d4bdf9980512e1dd1cd44375f5f3
Robin Melanson <remcogold@gmail.com>
2019-04-02 21:55:23 -0400

    Update installJava8.sh

--- installs/installJava8.sh
+++ installs/installJava8.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #------------------- DOWNLOAD AND INSTALL JAVA 8 AND MAKE DEFAULT --------------------
+echo Installing Java 1.8
 yum install java-1.8.0-openjdk-devel -y
 echo 2 | /usr/sbin/alternatives --config java
 echo 2 | /usr/sbin/alternatives --config javac
 echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0" >> /etc/profile.d/java.sh
+echo Java 1.8 Installed
