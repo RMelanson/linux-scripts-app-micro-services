16085bf7cb1a1a1d528a652d7f17d803cc828710
Robin Melanson <remcogold@gmail.com>
2019-04-05 14:58:49 -0400

    Update addAppServices.sh

--- installs/addAppServices.sh
+++ installs/addAppServices.sh
@@ -6,7 +6,7 @@ echo $scriptName Set $daemon as an init.d service
 scriptName=addAppServices.sh:
 
 # INSTALL JAVA APPS TO SERVER
-echo $setupName EXECUTING cp ./installs/appServices.d $appServicesDir/$daemon
+echo $setupName EXECUTING cp ./installs/$daemon.d $appServicesDir/$daemon
 echo y | cp ./installs/appServices.d $appServicesDir/$daemon
 chmod 766 $appServicesDir/$daemon
 
