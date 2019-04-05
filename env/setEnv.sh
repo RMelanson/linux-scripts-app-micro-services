ab98de2e52b0103cf951c20399e9059999021b44
Robin Melanson <remcogold@gmail.com>
2019-04-05 15:02:17 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -8,5 +8,5 @@ gitRepo="linux-scripts-app-services"
 installDir="/tmp/scripts/utils/$pkg"
 appServicesDir=/etc/init.d
 appProcessDir=$appServicesDir/appProcesses
-daemon=appServices.d
+daemon=apps
 testApp="java -jar $installDir/test/jars/springBootHelloWorldDemo-0.1.0.jar"
