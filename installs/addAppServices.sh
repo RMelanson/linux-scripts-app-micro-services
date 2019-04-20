26169f968a805ebd5ec019d28f539aef6eb40451
Robin Melanson <remcogold@gmail.com>
2019-04-20 12:22:44 -0400

    Update addAppServices.sh

--- installs/addAppServices.sh
+++ installs/addAppServices.sh
@@ -7,7 +7,7 @@ scriptName=addAppServices.sh:
 
 # INSTALL JAVA APPS TO SERVER
 echo $setupName EXECUTING cp ./installs/$daemon.d $appServicesDir/$daemon
-echo y | cp ./installs/appServices.d $appServicesDir/$daemon
+echo y | cp ./installs/$daemon.d $appServicesDir/$daemon
 chmod 766 $appServicesDir/$daemon
 
 echo $scriptName EXECUTING chkconfig $daemon on
