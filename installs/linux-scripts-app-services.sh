9161bd32038cd808d8a2b85e01cecc772fa1c113
Robin Melanson <remcogold@gmail.com>
2019-04-21 01:09:45 -0400

    Update and rename installJavaRestTestService.sh to linux-scripts-app-services.sh

--- installs/linux-scripts-app-services.sh
+++ installs/linux-scripts-app-services.sh
@@ -5,14 +5,13 @@ echo $setupName: EXECUTING ./env/setEnv.sh
 . ./env/setEnv.sh
 
 echo $setupName: EXECUTING mkdir $testServicesScripts
-mkdir $testServicesScripts
+mkdir -p $testServicesScripts
 
 echo $setupName: EXECUTING mkdir $testServicesJars
-mkdir $testServicesJars
+mkdir -p $testServicesJars
 
 echo $setupName: EXECUTING mkdir $testServicesJars
 cp $testApp $testServicesJars
 
-# INSTALL TEST APP SERVICE
-echo $setupName EXECUTING service $daemon start $testApp
-service $daemon start $testApp
+echo $setupName: EXECUTING mkdir $testServicesJars
+echo "java - jar $testServicesJars/springBootHelloWorldDemo-0.1.0.jar" > $testServicesScripts/springBootHelloWorldDemo-0.1.0.sh
