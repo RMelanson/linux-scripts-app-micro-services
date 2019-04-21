687ea3f756d8df0c2d010e68b960a88b46ae4f5e
Robin Melanson <remcogold@gmail.com>
2019-04-21 01:57:26 -0400

    Update linux-scripts-app-services.sh

--- installs/linux-scripts-app-services.sh
+++ installs/linux-scripts-app-services.sh
@@ -4,14 +4,14 @@ setupName=linux-scripts-app-services.sh:
 echo $setupName: EXECUTING ./env/setEnv.sh
 . ./env/setEnv.sh
 
-echo $setupName: EXECUTING mkdir $testServicesScripts
-mkdir -p $testServicesScripts
+echo $setupName: EXECUTING mkdir $testServicesScriptsDir
+mkdir -p $testServicesScriptsDir
 
-echo $setupName: EXECUTING mkdir $testServicesJars
-mkdir -p $testServicesJars
+echo $setupName: EXECUTING mkdir $testServicesJarsDir
+mkdir -p $testServicesJarsDir
 
-echo $setupName: EXECUTING mkdir cp $testApp $testServicesJars
-cp $testApp $testServicesJars
+echo $setupName: EXECUTING cp $testApp $testServicesJarsDir
+cp $testApp $testServicesJarsDir
 
-echo $setupName: EXECUTING echo "java - jar $testServicesJars/springBootHelloWorldDemo-0.1.0.jar" > $testServicesScripts/springBootHelloWorldDemo-0.1.0.
-echo "java - jar $testServicesJars/springBootHelloWorldDemo-0.1.0.jar" > $testServicesScripts/springBootHelloWorldDemo-0.1.0.sh
+echo $setupName: EXECUTING echo "java - jar $testServicesJarsDir/$testApp" > $testServicesScriptsDir/springBootHelloWorldDemo-0.1.0.
+echo "java - jar $testServicesJarsDir/$testApp" > $testServicesScriptsDir/springBootHelloWorldDemo-0.1.0.sh
