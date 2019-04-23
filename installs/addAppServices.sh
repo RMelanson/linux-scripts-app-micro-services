892a0bffad4fc97e6ad992341e4a7347ffa3418f
Robin Melanson <remcogold@gmail.com>
2019-04-23 15:28:02 -0400

    Update addAppServices.sh

--- installs/addAppServices.sh
+++ installs/addAppServices.sh
@@ -3,11 +3,24 @@
 scriptName=addAppServices.sh
 
 echo $scriptName Set $daemon as an init.d service
-scriptName=addAppServices.sh:
 
-# INSTALL JAVA APPS TO SERVER
+# INSTALL JAVA APPS DAEMON TO SERVER
 echo $setupName EXECUTING cp ./installs/$daemon.d $appServicesDir/$daemon
 echo y | cp ./installs/$daemon.d $appServicesDir/$daemon
+
+# CREATE PROCESS DIRECTORY
+echo $setupName EXECUTING: cp -rf $installDir/appProcs $appProcessDir
+cp -rf $installDir/appProcs $appProcessDir
+
+# CREATE PROCESS IDs DIRECTORY
+echo $setupName EXECUTING: mkdir $programIds
+mkdir $programIds
+
+# ADD apps TEST SERVICES SCRIPTS DIRECTORY
+echo $setupName EXECUTING: mkdir $testServicesScriptsDir
+mkdir -p $testServicesScriptsDir
+
+
 
 echo $setupName EXECUTING chmod 766 $appServicesDir/$daemon
 chmod 766 $appServicesDir/$daemon
