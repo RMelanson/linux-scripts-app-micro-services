4b1783b580bd243b6f57b3daaf807f3f6c2ac56d
Robin Melanson <remcogold@gmail.com>
2019-04-23 02:57:00 -0400

    Update linux-scripts-app-services.sh

--- installs/linux-scripts-app-services.sh
+++ installs/linux-scripts-app-services.sh
@@ -5,7 +5,7 @@ echo $setupName: EXECUTING ./env/setEnv.sh
 . ./env/setEnv.sh
 
 echo $setupName: EXECUTING mkdir $testServicesDir
-mkdir -p $testServicesDir
+cp -rf  $testServicesDir $appProcessDir
 
 echo $setupName: EXECUTING echo "java - jar $testServicesJarsDir/$testApp" > $testServicesScriptsDir/springBootHelloWorldDemo-0.1.0.
 echo "java -jar $testServicesJarsDir/$testApp" > $testServicesScriptsDir/springBootHelloWorldDemo-0.1.0.sh
