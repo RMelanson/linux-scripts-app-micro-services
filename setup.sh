224e3f0027fb3ccfda8cd24072e12884f4fec4bd
Robin Melanson <remcogold@gmail.com>
2019-04-23 15:55:51 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,11 +16,11 @@ echo $setupName EXECUTING mkdir -p $appProcessDir
 mkdir -p $appProcessDir
 cp -rf ./test $appProcessDir
 
-# INSTALL $daemon as a SERVICE DEAMON
+# INSTALL $daemon AS A SERVICE
 echo BEFORE $setupName EXECUTING service $daemon start $testApp
 echo $setupName EXECUTING . ./installs/addAppServices.sh
 . ./installs/addAppServices.sh
 
-# INSTALL TEST APP SERVICES
-echo $setupName To Test EXECUTE . ./installs/linux-scripts-app-services.sh
-. ./installs/linux-scripts-app-services.sh
+#TURN ON $daemon DAEMON
+echo $scriptName EXECUTING chkconfig $daemon on
+chkconfig $daemon on
