7cc537d0b06187b8a2d212f2c883354134c85a38
Robin Melanson <remcogold@gmail.com>
2019-04-24 10:54:13 -0400

    Update addAppServices.sh

--- installs/addAppServices.sh
+++ installs/addAppServices.sh
@@ -27,9 +27,5 @@ mkdir $programIds
 echo $setupName EXECUTING: mkdir $testServicesScriptsDir
 mkdir -p $testServicesScriptsDir
 
-
-echo $setupName: EXECUTING echo "java - jar $testServicesJarsDir/$testApp" > $testServicesScriptsDir/springBootHelloWorldDemo-0.1.0.
-echo "java -jar $testServicesJarsDir/$testApp" > $testServicesScriptsDir/springBootHelloWorldDemo-0.1.0.sh
-
 echo $setupName: EXECUTING chmod -R 744 $appProcessDir
 chmod -R 744 $appProcessDir
