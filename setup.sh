5db178b9a04d67037ec35190b4e3e1a6811ff2fe
Robin Melanson <remcogold@gmail.com>
2019-04-24 11:32:07 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,27 @@
 #! /bin/bash
 setupName=setup.sh:
 # SETUP THE REQUIRED ENVIRONMENTt
-echo setup.sh: EXECUTING ./env/setEnv.sh
+echo setup.sh: EXECUTING: ./env/setEnv.sh
 . ./env/setEnv.sh
 
 # COPY RESTORE 
 cp $pkg_RESTORE.sh ..
 
 #INSTALL JAVA 8
-echo Setup.sh:  EXECUTING . ./installs/installJava8.sh
+echo Setup.sh:  EXECUTING: . ./installs/installJava8.sh
 . ./installs/installJava8.sh
 
-# CREATE SERVICE APPLICATION DIRECTORY
-echo $setupName EXECUTING mkdir -p $appProcessDir
-mkdir -p $appProcessDir
-cp -rf ./test $appProcessDir
+# CREATE SERVICE APPLICATION DIRECTORIES
+echo $setupName EXECUTING" cp -rf ./test $appProcessDir
+cp -rf ./installs/appProcesses $appServicesDir
+
+# SETUP RECOVERY
+cp $reinstall.sh/reinstall.sh $parentDir
 
 # INSTALL $daemon AS A SERVICE
-echo BEFORE $setupName EXECUTING service $daemon start $testApp
-echo $setupName EXECUTING . ./installs/addAppServices.sh
+echo $setupName EXECUTING: . ./installs/addAppServices.sh
 . ./installs/addAppServices.sh
 
 #TURN ON $daemon DAEMON
-echo $scriptName EXECUTING chkconfig $daemon on
+echo $scriptName EXECUTING: chkconfig $daemon on
 chkconfig $daemon on
