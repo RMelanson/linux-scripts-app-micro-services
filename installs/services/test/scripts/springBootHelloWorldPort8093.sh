13a2d83f6417b23c728d6c99e5788e214f494ca3
Robin Melanson <remcogold@gmail.com>
2019-05-04 20:34:11 -0400

    Update springBootHelloWorldPort8093.sh

--- installs/services/test/scripts/springBootHelloWorldPort8093.sh
+++ installs/services/test/scripts/springBootHelloWorldPort8093.sh
@@ -1,2 +1,1 @@
-port=8093
-java -jar /etc/init.d/services/test/jars/springBootHelloWorldDemo-0.1.0.jar -–server.port=$port
+java -jar /etc/init.d/services/test/jars/springBootHelloWorldDemo-0.1.0.jar -–server.port=8093
