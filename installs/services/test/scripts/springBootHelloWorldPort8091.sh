bf98f3e95a019c6ff65f99d5d470dc59ee9d6d54
Robin Melanson <remcogold@gmail.com>
2019-07-05 15:16:20 -0400

    Update springBootHelloWorldPort8091.sh

--- installs/services/test/scripts/springBootHelloWorldPort8091.sh
+++ installs/services/test/scripts/springBootHelloWorldPort8091.sh
@@ -1,1 +1,1 @@
-java -jar /etc/init.d/services/test/jars/springBootHelloWorldDemo-0.1.0.jar --server.port=8091
+java -jar /etc/init.d/services/test/apps/libs/springBootHelloWorldDemo-0.1.0.jar --server.port=8091
