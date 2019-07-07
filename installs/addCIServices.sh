4655366166f38231dea88987f581ebf0ac0fe677
Robin Melanson <remcogold@gmail.com>
2019-07-07 03:20:50 -0400

    Update addCIServices.sh

--- installs/addCIServices.sh
+++ installs/addCIServices.sh
@@ -8,12 +8,12 @@ echo addCIServices.sh EXECUTING: cp -rf ./installs/CI $ciParentDir
 echo y | cp -rf ./installs/CI $ciParentDir
 
 # REMOVE OLD CLOUD INITIALISER DAEMON
-echo addCIServices.sh EXECUTING: rm /sbin/$daemon
-rm /sbin/$daemon
+echo addCIServices.sh EXECUTING: rm /sbin/$daemon.d
+rm /sbin/$daemon.d
 
 # CREATE SYMBOLIC LINK FOR APPS SERVICES
-echo addCIServices.sh EXECUTING: ln -s  /sbin/$daemon $ciBin/$daemon
-ln -s  $ciBin/$daemon /sbin/$daemon
+echo addCIServices.sh EXECUTING: ln -s  /sbin/$daemon $ciBin/$daemon.d
+ln -s  $ciBin/$daemon /sbin/$daemon.d
 
 echo addCIServices.sh EXECUTING: chmod 755 $sbin/$daemon
 chmod 755 $sbin/$daemon
