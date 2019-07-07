8af0a451ad82ff68e379e21ae6a654bb8cf3bddb
Robin Melanson <remcogold@gmail.com>
2019-07-07 03:02:25 -0400

    Update addCIServices.sh

--- installs/addCIServices.sh
+++ installs/addCIServices.sh
@@ -3,10 +3,6 @@
 echo addCIServices.sh EXECUTING ./env/setEnv.sh
 . ./env/setEnv.sh
 
-#INSTALL $daemon AS AN init.d SERVICE
-
-echo addCIServices.sh Set $daemon as an Cloud Initialiser service
-
 # INSTALL JAVA APPS DAEMON TO SERVER
 echo addCIServices.sh EXECUTING: cp -rf ./installs/CI $ciBin
 echo y | cp -rf ./installs/CI $ciBin
@@ -21,10 +17,6 @@ ln -s  $ciBin/$daemon /sbin/$daemon
 
 echo addCIServices.sh EXECUTING: chmod 755 $sbin/$daemon
 chmod 755 $sbin/$daemon
-
-# CREATE PROCESS IDs DIRECTORY
-echo addCIServices.sh EXECUTING: mkdir $programIds
-mkdir $programIds
 
 echo addCIServices.sh EXECUTING: chmod -R 755 $servicesDir
 chmod -R 755 $servicesDir
