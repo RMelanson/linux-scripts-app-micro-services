5cfe1a437380b1c6fad03b0beabe6743451d4351
Robin Melanson <remcogold@gmail.com>
2019-07-13 03:04:09 -0400

    Update startCloudInitialiserService.sh

--- installs/CI/bin/startCloudInitialiserService.sh
+++ installs/CI/bin/startCloudInitialiserService.sh
@@ -1,3 +1,3 @@
 jarFile=$appServicesDir/jobs/java/libs/cloud-initialiser-0.1.0.jar
-echo "EXECUTING startCloudInitialiserService.sh: $daemon start java $serverPort -jar $jarFile
+echo "EXECUTING startCloudInitialiserService.sh: $daemon start java $serverPort -jar $jarFile"
 $daemon start java $serverPort -jar $jarFile
