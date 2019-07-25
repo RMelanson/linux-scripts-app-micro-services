82645eb7c023af253f88fc6b76e9da589bf81586
Robin <robin.e.melanson@gmail.com>
2019-07-25 12:26:39 -0400

    wip

---
+++ installs/webapp/cloudinitialiser/ciBuilder/js/ciBuilder.js
@@ -0,0 +1,22 @@
+	function buildURL() {
+		var url = "";
+		url += document.getElementById("protocol").value;
+		url += "://";
+		url += document.getElementById("host").value;
+		url += ":";
+		url += document.getElementById("port").value;
+		url += "/";
+		url += document.getElementById("service").value;
+		url += "?";
+        url += document.getElementById("command").value;
+		url += "=";
+        url += document.getElementById("parameter").value;
+       
+		//var i;
+		// for (i = 2; i < x.length; i++) {
+		// 	url += x.elements[i].value + "/";
+		// }
+/* 		TO TO addTab(title, url) */
+//document.getElementById("url").value = url.link(url);
+document.getElementById("url").value = url;
+}
