bf27c32f3d4d7cb59f34b7c065e02726c9e6f9b0
Robin <robin.e.melanson@gmail.com>
2019-07-29 02:04:58 -0400

    WIP

--- installs/webapp/cloudinitialiser/ciBuilder/js/ajaxRestCalls.js
+++ installs/webapp/cloudinitialiser/ciBuilder/js/ajaxRestCalls.js
@@ -1,27 +1,63 @@
-function ajaxRestGet(restURL) {
-	alert("ajaxRestGet(" + restURL + ")");
+function ajaxGetJson(url) {
+	var response;
+	var status;
+	var data;
+
 	$.support.cors = true;
-	var response = ajaxGet(restURL);
-	alert("TESTING \ajaxRestGet(" + restURL + ")\n status = " + status + "\nresponse = \n" + response);
+	$.getJSON(url, data, function (data, status) {
+		if (status === "success") {
+			console.log("data = "+data);
+			console.log("GetJSON \ajaxGetJson(" + url + ")\n" + url + "\nWORKS status = " + status + "\ndata = \n" + data);
+			alert ("1 " + data);
+			alert ("2 " + JSON.stringify(data));
+		}
+		else {
+		    console.log("GetJSON ERROR \ajaxGetJson(" + url + ")\n" + url + "\nWORKS status = " + status + "\ndata = \n" + data);
+		}
+	    console.log(data);
+	    }
+	);
 }
 
 function ajaxGet(restURL) {
 	var response;
-	jQuery.ajax({
-		type: "GET",
+	$.ajax({
 		url: restURL,
-		dataType: "text",
+		type: "GET",
+		dataType: 'json',
+		contentType: "application/json",
 		success: function (data, status, jqXHR) {
 			response = data;
-			alert("TESTING \ajaxRestGet(" + restURL + ")\n" + restURL + "\nWORKS status = " + status + "\ndata = \n" + data);
+			alert("SUCCESS \ajaxGet(" + restURL + ")\n" + restURL + "\nWORKS status = " + status + "\ndata = \n" + data);
 		},
-		error: function (xhr, ajaxOptions, thrownError) {
-			response = "ERROR \ajaxRestGet(" + restURL + ")\n" + restURL + "\nFAILED status = " + xhr.status;
-			alert("response\n"+thrownError);
+		error: function (xhr, ajaxOptions, errorThrown) {
+			response = "ERROR \ajaxGet(" + restURL + ")\n" + restURL + "\nFAILED status = " + xhr.status;
+			alert("ERROR response\n" + response);
+			console.log(errorThrown);
 		}
 	});
 	return response;
 }
+
+function ajaxPost(restURL) {
+	var response;
+	$.ajax({
+		url: 'users.php',
+		dataType: 'json',
+		type: 'POST',
+		contentType: 'application/json',
+		data: JSON.stringify({ "first-name": $('#first-name').val(), "last-name": $('#last-name').val() }),
+		processData: false,
+		success: function (data, textStatus, jQxhr) {
+			$('#response pre').html(JSON.stringify(data));
+		},
+		error: function (jqXhr, textStatus, errorThrown) {
+			console.log(errorThrown);
+		}
+	});
+	return response;
+}
+
 
 function tutorialTest(restURL) {
 	$(document).ready(function () {
@@ -32,4 +68,18 @@ function tutorialTest(restURL) {
 			$('.greeting-content').append(data.content);
 		});
 	});
+}
+
+function loadW3Doc(restURL) {
+	alert("loadW3Doc(restURL)");
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			alert(this.responseText);
+			//      document.getElementById("demo").innerHTML =
+			this.responseText;
+		}
+	};
+	xhttp.open("GET", restURL, true);
+	xhttp.send();
 }
--- installs/webapp/cloudinitialiser/ciBuilder/js/ciBuilder.js
+++ installs/webapp/cloudinitialiser/ciBuilder/js/ciBuilder.js
@@ -1,16 +1,16 @@
-	function buildURL() {
-		var url = "";
-		url += document.getElementById("protocol").value;
-		url += "://";
-		url += document.getElementById("host").value;
-		url += ":";
-		url += document.getElementById("port").value;
-		url += "/";
-		url += document.getElementById("service").value;
-		url += "?";
-        url += document.getElementById("command").value;
-		url += "=";
-        url += document.getElementById("parameter").value;
+	// function buildURL() {
+	// 	var url = "";
+	// 	url += document.getElementById("protocol").value;
+	// 	url += "://";
+	// 	url += document.getElementById("host").value;
+	// 	url += ":";
+	// 	url += document.getElementById("port").value;
+	// 	url += "/";
+	// 	url += document.getElementById("service").value;
+	// 	url += "?";
+    //     url += document.getElementById("command").value;
+	// 	url += "=";
+    //     url += document.getElementById("parameter").value;
        
 		//var i;
 		// for (i = 2; i < x.length; i++) {
@@ -18,5 +18,5 @@
 		// }
 /* 		TO TO addTab(title, url) */
 //document.getElementById("url").value = url.link(url);
-document.getElementById("url").value = url;
+// document.getElementById("url").value = url;
 }
