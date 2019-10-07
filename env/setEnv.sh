3718be4996cab008b9e207a8ffb8ca965efe1e36
Robin Melanson <robin.e.melanson@gmail.com>
2019-10-06 21:43:24 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # LINUX SCRIPTS CONFIGURATION PARAMETERS
-bootstrap="/cloudInitialiserBootStrap.sh"
-gitRepo="/cloudInitialiserBootStrap."
+bootstrap="/cloudInitializerBootStrap.sh"
+gitRepo="/cloudInitializerBootStrap."
 
 # SET UP INSTALLATION DIRECTORY
 pkg=APP_SERVICES
@@ -11,7 +11,7 @@ parentDir="/tmp/scripts/$scriptType"
 pkgDir="$parentDir/$pkg"
 installDir="$pkgDir/installs"
 
-# SET UP CLOUD INITIALISER PROGRAM DIRECTORY
+# SET UP CLOUD INITIALIZER PROGRAM DIRECTORY
 ciParentDir=/opt
 ciDir=/$ciParentDir/CI
 ciBin=$ciDir/bin
