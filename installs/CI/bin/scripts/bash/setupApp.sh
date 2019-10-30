c49c8e4aaeec3464e21410b78072fe6cf1f4ab1a
Robin <robin.e.melanson@gmail.com>
2019-10-30 03:05:59 -0400

    WIP

--- installs/CI/bin/scripts/bash/setupApp.sh
+++ installs/CI/bin/scripts/bash/setupApp.sh
@@ -1,7 +1,12 @@
+echo "BEFORE EXECUTING: Parms \$\*  = $*" | tee $dir/setup.log
+echo "BEFORE EXECUTING: Parms \@*\* = @*" | tee -a $dir/setup.log
 dir=$1
+shift
+echo "AFTER EXECUTING: Parms \$\*  = $*" | tee -a $dir/setup.log
+echo "AFTER EXECUTING: Parms \@*\* = @*" | tee -a $dir/setup.log
 mode=744
-echo "EXECUTING: cd $dir " > $dir/setup.log
+echo "EXECUTING: cd $dir " | tee -a $dir/setup.log
 cd $dir
-echo "find $dir -name '*.sh' -exec chmod $mode {} \;" >> $dir/setup.log
+echo "find $dir -name '*.sh' -exec chmod $mode {} \;" | tee -a $dir/setup.log
 find $dir -name '*.sh' -exec chmod $mode {} \;
 ./setup.sh $* 2>&1| tee -a setup.log
