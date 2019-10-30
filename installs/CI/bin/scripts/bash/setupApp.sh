ecb93b8898fabc1b75e76b62a7cc23d19367545c
Robin <robin.e.melanson@gmail.com>
2019-10-30 03:50:36 -0400

    WIP

--- installs/CI/bin/scripts/bash/setupApp.sh
+++ installs/CI/bin/scripts/bash/setupApp.sh
@@ -1,12 +1,21 @@
-echo "BEFORE EXECUTING: Parms \$\*  = $*" | tee $dir/setup.log
-echo "BEFORE EXECUTING: Parms \@*\* = @*" | tee -a $dir/setup.log
-dir=$1
+#Sample install Call
+#http://99.79.59.230:9090/ci/api/v1/install/wordpress?siteName=MyNewSite&port=9111&adminId=MyId&adminPWD=password&installDir=/var
+CI_HOME="opt/CI"LOGS=$CI_HOME/logs
+BOOTSTRAPS=$CI_HOME/bootstraps
+APP_SUB_DIR=$1
+APP_BOOTSTRAP=$BOOTSTRAPS/$APP_SUB_DIR
+APP_LOGS=$LOGS/$APP_SUB_DIR
 shift
-echo "AFTER EXECUTING: Parms \$\*  = $*" | tee -a $dir/setup.log
-echo "AFTER EXECUTING: Parms \@*\* = @*" | tee -a $dir/setup.log
-mode=744
-echo "EXECUTING: cd $dir " | tee -a $dir/setup.log
-cd $dir
-echo "find $dir -name '*.sh' -exec chmod $mode {} \;" | tee -a $dir/setup.log
-find $dir -name '*.sh' -exec chmod $mode {} \;
-./setup.sh $* 2>&1| tee -a setup.log
+PARMS=$*
+MODE=744
+
+echo "APP_SUB_DIR  = $APP_SUB_DIR"| tee $APP_LOGS/setup.log
+echo "APP_BOOTSTRAP  = $APP_BOOTSTRAP" | tee -a $APP_LOGS/setup.log
+echo "APP_LOGS  = $APP_LOGS" | tee -a $APP_LOGS/setup.log
+
+echo "find $APP_BOOTSTRAP -name '*.sh' -exec chmod $MODE {} \;" | tee -a $APP_LOGS/setup.log
+
+find $APP_BOOTSTRAP -name '*.sh' -exec chmod $MODE {} \;
+
+cd $APP_BOOTSTRAP
+./setup.sh $PARMS 2>&1| tee -a $APP_LOGS/setup.log
