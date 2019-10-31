4e6c5d24dad43e96b41254357c7fb9b1999fc82e
Robin <robin.e.melanson@gmail.com>
2019-10-31 00:37:19 -0400

    WIP

--- installs/CI/bin/scripts/bash/setupApp.sh
+++ installs/CI/bin/scripts/bash/setupApp.sh
@@ -1,16 +1,18 @@
 #Sample install Call
 #http://99.79.59.230:9090/ci/api/v1/install/wordpress?siteName=MyNewSite&port=9111&adminId=MyId&adminPWD=password&installDir=/var
 CI_HOME="/opt/CI"
-LOGS=$CI_HOME/logs
 BOOTSTRAPS=$CI_HOME/bootstraps
-APP_SUB_DIR=$1
+LOGS=$CI_HOME/logs
+APP_REPO=$1
+APP_SUB_DIR=$(echo "$1" | sed 's/-/\//g') 
 APP_BOOTSTRAP=$BOOTSTRAPS/$APP_SUB_DIR
 APP_LOGS=$LOGS/$APP_SUB_DIR
 shift
 PARMS=$*
 MODE=744
 
-echo "APP_SUB_DIR  = $APP_SUB_DIR"| tee $APP_LOGS/setup.log
+echo "APP_REPO  = $APP_REPO" | tee $APP_LOGS/setup.log
+echo "APP_SUB_DIR  = $APP_SUB_DIR" | tee $APP_LOGS/setup.log
 echo "APP_BOOTSTRAP  = $APP_BOOTSTRAP" | tee -a $APP_LOGS/setup.log
 echo "APP_LOGS  = $APP_LOGS" | tee -a $APP_LOGS/setup.log
 
