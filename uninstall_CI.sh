898a9aeb18c8869caa52b507993b178cf17eccec
Robin Melanson <robin.e.melanson@gmail.com>
2019-11-11 00:35:11 -0500

    Update uninstall_CI.sh

--- uninstall_CI.sh
+++ uninstall_CI.sh
@@ -2,6 +2,5 @@ clear
 cloudinit stop all
 cloudinit clean all
 #rm -rf /tmp/scripts/utils/APP_SERVICES
-rm -rf ./CLOUD_INITIALIZER
-rm -rf /opt/CI
+rm -rf /opt/CI/CLOUD_INITIALIZER
 rm -rf /var/www/html/cloudinitializer/
