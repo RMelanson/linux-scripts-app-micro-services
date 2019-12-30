c0ab0228818a9866c9ef1a98ca895c3804e998e1
Robin Melanson <robin.e.melanson@gmail.com>
2019-12-29 20:27:43 -0500

    Update uninstall.sh

--- installs/uninstall.sh
+++ installs/uninstall.sh
@@ -3,6 +3,8 @@ cloudinit stop all
 cloudinit clean all
 bootstraps="/opt/CI/bin/scripts/bash/bootstraps"
 apps="$bootstraps/apps"
-pkg=$apps/CLOUD_INITIALIZER
+pkg=$apps/CI
+echo "uninstall_CI.sh ECECUTING: rm -rf $pkg"
 rm -rf $pkg
+echo "uninstall_CI.sh ECECUTING: rm -rf /var/www/html/cloudinitializer/"
 rm -rf /var/www/html/cloudinitializer/
