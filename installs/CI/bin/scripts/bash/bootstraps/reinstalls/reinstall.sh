3b18dc35b8a5848c058a63fa1145a6af48f76a2f
Robin Melanson <robin.e.melanson@gmail.com>
2019-12-30 02:23:57 -0500

    Update reinstall.sh

--- installs/CI/bin/scripts/bash/bootstraps/reinstalls/reinstall.sh
+++ installs/CI/bin/scripts/bash/bootstraps/reinstalls/reinstall.sh
@@ -3,8 +3,8 @@ pkg=$2
 bootStrapDir="/opt/CI/bin/scripts/bash/bootstraps"
 pkgDir="$scriptType/$pkg"
 
-echo "reinstall.sh EXECUTING: . ../uninstall_$pkg.sh"
-. ../uninstall_$pkg.sh
+echo "reinstall.sh EXECUTING: . ../uninstalls/uninstall_$pkg.sh"
+ . ../uninstalls/uninstall_$pkg.sh
 
 echo "reinstall.sh EXECUTING: . /opt/CI/bin/scripts/bash/ciUtils/setupApp.sh $pkgDir"
 . /opt/CI/bin/scripts/bash/ciUtils/setupApp.sh $pkgDir
