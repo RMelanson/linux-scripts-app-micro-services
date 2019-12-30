7aeba58db30d0091cf8f9fc85d8234e8b8207d1c
Robin Melanson <robin.e.melanson@gmail.com>
2019-12-29 20:39:56 -0500

    Update reinstallApp.sh

--- installs/CI/bin/scripts/bash/bootstraps/reinstalls/reinstallApp.sh
+++ installs/CI/bin/scripts/bash/bootstraps/reinstalls/reinstallApp.sh
@@ -1,13 +1,12 @@
 App=$1
 if [ -z $1 ]; then
    echo "ERROR APP Not Specified"
-   return 0;
 else
    FILE=reinstall_$1.sh
    if [ -f "$FILE" ]; then
       echo "EXECUTING . ./$FILE"
       . ./$FILE;
    else 
-      echo ""ERROR $FILE does not exist"
+      echo "ERROR $FILE does not exist"
    fi
 fi
