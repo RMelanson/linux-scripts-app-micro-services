0aca63b55ffbc4da4f1140c2e1c7e70f5a174552
Robin Melanson <robin.e.melanson@gmail.com>
2020-01-01 19:05:20 -0500

    Update reinstall.sh

--- installs/CI/bin/scripts/bash/bootstraps/reinstalls/reinstall.sh
+++ installs/CI/bin/scripts/bash/bootstraps/reinstalls/reinstall.sh
@@ -5,6 +5,9 @@ pkgDir="$scriptType/$pkg"
 
 echo "reinstall.sh EXECUTING: . ../uninstalls/uninstall_$pkg.sh"
  . ../uninstalls/uninstall_$pkg.sh
+ 
+echo "reinstall.sh EXECUTING: . ../uninstalls/clone_$pkg.sh"
+ . ../uninstalls/clone_$pkg.sh
 
 echo "reinstall.sh EXECUTING: . /opt/CI/bin/scripts/bash/ciUtils/setupApp.sh $pkgDir"
 . /opt/CI/bin/scripts/bash/ciUtils/setupApp.sh $pkgDir
